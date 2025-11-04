a40951de0e4fc6c486fd5e39f72ecb0c834ac854
arc-1409 <lulu.august.liao@gmail.com>
2025-11-03 23:45:12 -0500

    attempt 5: move result to with found

--- algorithms.js
+++ algorithms.js
@@ -79,6 +79,7 @@ async function recentMatch(page, obj) {
     }
 
     let found = false;
+    let result = "win";
 
     // filter leagues
     if(obj.league === "Premier League") {
@@ -110,7 +111,6 @@ async function recentMatch(page, obj) {
 
     async function scrape(url, leagueResult) {
         await page.goto(url, { waitUntil: "networkidle2"});
-        let result = "win";
         const teams = await page.$$eval("tr[class*='CellsRow']", rows => {
             return rows.map(row => {
                 // documentation: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll 
