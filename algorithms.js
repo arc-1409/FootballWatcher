393d02bc605f3ad01d954a5833658e517c4cd025
arc-1409 <lulu.august.liao@gmail.com>
2025-11-03 23:47:41 -0500

    success: use targetTeam.result

--- algorithms.js
+++ algorithms.js
@@ -79,7 +79,6 @@ async function recentMatch(page, obj) {
     }
 
     let found = false;
-    let result = "win";
 
     // filter leagues
     if(obj.league === "Premier League") {
@@ -123,6 +122,7 @@ async function recentMatch(page, obj) {
                     resultArray.push(el.textContent.trim());
                 });
 
+                let result = "win";
                 if(resultArray[5] === "Result Loss") {
                     result = 'loss';
                 } else if (resultArray[5] === "Result Draw") {
@@ -141,7 +141,7 @@ async function recentMatch(page, obj) {
 
         if(targetTeam) {
             console.log(`${obj.team}'s most recent game at ${leagueResult} with /team/ was a ${targetTeam.result}.`);
-            if (result === 'win') {
+            if (targetTeam.result === 'win') {
                 console.log("congratulations!");
             }
             found = true;
