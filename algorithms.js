642113e19041581186de7cc1e348997bf0cfeb65
arc-1409 <lulu.august.liao@gmail.com>
2025-11-07 16:40:41 -0500

    finish workflow

--- algorithms.js
+++ algorithms.js
@@ -150,23 +150,6 @@ async function recentMatch(page, obj) {
     }
 
     // scrape algorithm 1
-
-    /*
-    workflow
-    1. scrapeIntel runs with current time obj
-    2. scan for league names ("Premier League", "La Liga", "German Bundesliga")
-    3. check if league name found 
-        a. call updateURL if not found
-        b. modify link by yyyy-mm
-        c. update time obj
-        d. pass obj (with updated link) back to scrapeIntel
-        e. start from step 1.
-    4. find opponent name (bonus: three-letter code?)
-    5. find match date and convert to lowercase
-    6. find fixture and convert to (team - opponent) format
-    7. create intel obj and store date, opponent, fixture attributes
-    */
-
     async function scrapeIntel(time, leagueResult) {
         await page.goto(time.intelurl, {waitUntil: "networkidle2"});
         await page.$$eval("class='ssrcss-7k0b15-HeaderWrapper'", rows => {
@@ -201,6 +184,7 @@ async function recentMatch(page, obj) {
                 return {inteldate, intelopp, intelfixture};
             })
         })
+
         const intel = {
             date: intelsearch.inteldate,
             opponent: intelsearch.intelopp,
