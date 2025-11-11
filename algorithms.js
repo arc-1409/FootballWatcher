fedb20ac8bf7e42221da61bc88cdec5890cfdefc
arc-1409 <lulu.august.liao@gmail.com>
2025-11-10 23:24:22 -0500

    fix 2: fix scrape 1 call params

--- algorithms.js
+++ algorithms.js
@@ -185,7 +185,7 @@ async function recentMatch(page, obj) {
 
     // scrape algorithm 2
     async function scrapeResult(url, time, intel, leagueResult) {
-        await scrapeIntel(time, leagueResult);              // debug: should return intel; referenceerror intel is not defined
+        await scrapeIntel(time, intel, leagueResult);              // debug: should return intel; referenceerror intel is not defined
         await page.goto(url, { waitUntil: "networkidle2"});
         const teams = await page.$$eval("tr[class*='CellsRow']", rows => {
             return rows.map(row => {
